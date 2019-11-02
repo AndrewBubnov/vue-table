4d70d87e76b5d06ab39920293eb1a7f5622289f0
AndrewBubnov <andrew.bubnov75@gmail.com>
2019-11-02 17:22:01 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,83 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div class="container">
+            <div class="row header">
+                <div class="record">Name</div>
+                <div class="record">Height</div>
+                <div class="record">Mass</div>
+                <div class="record">Hair color</div>
+                <div class="record">Skin color</div>
+                <div class="record">Eye color</div>
+                <div class="record">Birth year</div>
+                <div class="record">Gender</div>
+            </div>
+            <ListItem v-for="hero of list"
+                      :hero="hero"
+                      :key="hero.id"
+                      @onDelete="onDelete(hero.name)"
+            />
+        </div>
     </div>
-    <router-view/>
-  </div>
 </template>
 
+<script>
+import ListItem from './components/ListItem.vue'
+import heroList from "@/assets/heroList.ts";
+
+
+export default {
+  name: 'app',
+  components: {
+    ListItem: ListItem
+  },
+    mounted(){
+        this.list = this.list.map(hero => ({...hero, id: `f${(~~(Math.random()*1e8)).toString(16)}`}))
+    },
+    methods:{
+      onDelete(name){
+          this.list = this.list.filter(hero => hero.name !== name);
+      }
+    },
+  data: function () {
+    return {
+      list: heroList
+    }
+  },
+}
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 
-#nav {
-  padding: 30px;
-}
+    .container {
+        max-width: 1600px;
+        margin: auto;
+    }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    .row {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        height: 20px;
+        margin-bottom: 10px;
+    }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    .record {
+        width: 180px;
+        text-align: center;
+    }
+
+    .header {
+        width: 1232px;
+        font-weight: 600;
+        margin-bottom: 20px;
+    }
 </style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="row">
+      <div class="record" :key="name" v-for="(value, name) in userObj">{{ value }}</div>
+      <button @click="onDelete">Delete</button>
+    </div>
+</template>
+<script>
+export default {
+  name: 'ListItem',
+  props: {
+    hero: Object
+  },
+  data: function () {
+    return{
+      userObj: this.hero
+    }
+  },
+  methods: {
+    onDelete(){
+      this.$emit('onDelete')
+    }
+  },
+}
+</script>
+
+
+<style scoped>
+
+</style>
